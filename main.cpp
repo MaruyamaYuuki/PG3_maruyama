563d940b82a6c5015fc4050c103f9a7e2dbf8a45
MaruyamaYuuki <k023g0106@g.neec.ac.jp>
2024-10-29 12:05:22 +0900

    不要なクラスの削除

--- main.cpp
+++ main.cpp
@@ -1,31 +1,7 @@
 #include <stdio.h>
-#include "Chocolate.h"
-#include "WhiteChoco.h"
-#include "MilkChoco.h"
-#include "BitterChoco.h"
 
 int main() {
-	Chocolate* choco[3];
 
-	for (int i = 0; i < 3; i++) {
-		if (i == 0) {
-			choco[i] = new WhiteChoco;
-		}
-		else if (i == 1) {
-			choco[i] = new MilkChoco;
-		}
-		else if (i == 2) {
-			choco[i] = new BitterChoco;
-		}
-	}
-
-	for (int i = 0; i < 3; i++) {
-		choco[i]->DrawName();
-	}
-
-	for (int i = 0; i < 3; i++) {
-		delete choco[i];
-	}
 
 	return 0;
 }
